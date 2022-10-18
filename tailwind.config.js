142ed6532ccee1dce5247effdb9a9502d401ca16
Youssef Nassim <ynassim7@gmail.com>
2022-10-18 02:10:28 +0200

    fix tailwind config & better back arrow svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./_posts/**/*.mdx",
+    "./utils/**/*.{js,ts}",
   ],
   theme: {
     extend: {
